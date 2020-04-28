d2d6ae1aa8cecfb8538bea9a42863787640ed572
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2020-04-28 10:02:24 +0200

    Generalization and refactor of Detector Tomography module.

--- DetectorTomography.py
+++ DetectorTomography.py
@@ -44,136 +44,45 @@ def gtc_matrix_product_counting_function(arguments: list):
     return result
 
 
-class DetectorTomographyFitter:
-    """
-        This class is meant to resemble qiskit's state tomography and process tomography fitters and to calculate the
-        maximum likelihood povm estimator describing a detector basing on QDT job results and used probe states.
-    """
+class QDTCalibrationSetup:
 
-    def __init__(self, algorithm_convergence_threshold=1e-6):
-        self.algorithmConvergenceThreshold = algorithm_convergence_threshold
+    def __init__(self, qubits_number: int, probe_kets: List[np.array], frequencies_array: np.ndarray ):
+        self.qubits_number = qubits_number
+        self.probe_kets = probe_kets
+        self.probe_states = self.__get_probe_states(qubits_number, probe_kets)
+        self.frequencies_array = frequencies_array
 
-    def get_maximum_likelihood_povm_estimator(self, results_list: List[Result],
-                                              probe_kets: List[np.array], qiskit_register_convention=True) \
-            -> List[np.ndarray]:
+    @classmethod
+    def from_qiskit_results(cls, results_list: List[Result], probe_kets: List[np.array]):
         """
         Description:
-            Given results of Quantum Detector Tomography experiments and list of probe states, return the Maximum
-            Likelihood estimation of POVM describing a detector. Uses recursive method from [1]. See also [2].
-
-        Parameters:
-            :param results_list: List of results obtained from executing qiskit jobs.
-            :param probe_kets: A set of probe kets used to perform tomography.
-            :param qiskit_register_convention: Qiskit register convention is reverse to what we usually work with.
-            This solves the problem.
-
-        Returns
-            Maximum likelihood estimator of POVM describing a detector.
+            This method generates Calibration setup objects directly from qiskit job results and probe kets used
+            to generate circuits for these jobs. This method should be interpreted as sort of additional constructor
+            for qiskit users.
+        :param results_list: List of qiskit jobs results. In case of single job result it should still be a list.
+        :param probe_kets: Prove kets (in form of list of np.arrays) used to generate calibration circuits.
+        :return: Instance of QDT calibration setup from given job.
         """
-
-        frequencies_array = self.__get_frequencies_array_from_results(results_list, qiskit_register_convention)
-
-        # FROM THIS MOMENT IT WILL BE REMOVED WHEN GENERAL METHODS ARE CREATED!
-        probe_states = self.__get_probe_states(results_list, probe_kets)
-        number_of_probe_states = frequencies_array.shape[1]
-        dimension = probe_states[0].shape[0]
-
-        povm = []
-
-        for j in range(number_of_probe_states):
-            povm.append(np.identity(dimension) / number_of_probe_states)
-
-        # Threshold is dynamic, thus another variable
-        threshold = self.algorithmConvergenceThreshold
-
-        i = 0
-        current_difference = 1
-        while abs(current_difference) >= threshold:
-            i += 1
-
-            if i % 50 == 0:
-                last_step_povm = copy.copy(povm)
-
-            r_matrices = [self.__get_r_operator(povm[j], j, frequencies_array, probe_states)
-                          for j in range(number_of_probe_states)]
-            lagrange_matrix = self.__get_lagrange_matrix(r_matrices, povm)
-            povm = [self.__calculate_symmetric_m(lagrange_matrix, r_matrices[j], povm[j])
-                    for j in range(number_of_probe_states)]
-
-            if i % 50 == 0:  # calculate the convergence test only sometimes to make the code faster
-                current_difference = sum([np.linalg.norm(povm[k] - last_step_povm[k], ord=2)
-                                          for k in range(number_of_probe_states)])
-
-            elif i > 5e5:  # make sure it does not take too long, sometimes convergence might not be so good
-                threshold = 1e-3
-
-            elif i > 1e5:  # make sure it does not take too long, sometimes convergence might not be so good
-                threshold = 1e-4
-
-        return povm
-
-    def __get_maximum_likelihood_povm_from_frequencies(self, frequencies_array: np.ndarray,
-                                                       probe_kets: List[np.array]) \
-            -> List[np.ndarray]:
-
-        probe_states = self.__get_probe_states(results_list, probe_kets)
-        number_of_probe_states = frequencies_array.shape[1]
-        dimension = probe_states[0].shape[0]
-
-        povm = []
-
-        for j in range(number_of_probe_states):
-            povm.append(np.identity(dimension) / number_of_probe_states)
-
-        # Threshold is dynamic, thus another variable
-        threshold = self.algorithmConvergenceThreshold
-
-        i = 0
-        current_difference = 1
-        while abs(current_difference) >= threshold:
-            i += 1
-
-            if i % 50 == 0:
-                last_step_povm = copy.copy(povm)
-
-            r_matrices = [self.__get_r_operator(povm[j], j, frequencies_array, probe_states)
-                          for j in range(number_of_probe_states)]
-            lagrange_matrix = self.__get_lagrange_matrix(r_matrices, povm)
-            povm = [self.__calculate_symmetric_m(lagrange_matrix, r_matrices[j], povm[j])
-                    for j in range(number_of_probe_states)]
-
-            if i % 50 == 0:  # calculate the convergence test only sometimes to make the code faster
-                current_difference = sum([np.linalg.norm(povm[k] - last_step_povm[k], ord=2)
-                                          for k in range(number_of_probe_states)])
-
-            elif i > 5e5:  # make sure it does not take too long, sometimes convergence might not be so good
-                threshold = 1e-3
-
-            elif i > 1e5:  # make sure it does not take too long, sometimes convergence might not be so good
-                threshold = 1e-4
-
-        return povm
+        frequencies_array = cls.__get_frequencies_array_from_results(results_list)
+        # This qubits_number calculation is a little elaborate, but necessary.
+        circuits_number = sum(len(results.results) for results in results_list)
+        qubits_number = int(log(circuits_number, len(probe_kets)))
+        return cls(qubits_number, probe_kets, frequencies_array)
 
     @staticmethod
-    def __get_probe_states(results_list: List[Result],
-                           probe_kets: List[np.array]) -> List[np.ndarray]:
+    def __get_probe_states(qubits_number: int, probe_kets: List[np.array]) -> List[np.ndarray]:
         """
         Description:
             This method generates probe states (density matrix) from results and kets
             passed to maximum likelihood POVM counting object.
         Parameters:
-            :param results_list: List of results obtained from qiskit jobs execution.
+            :param qubits_number: Number of qubits used in the calibration experiments.
             :param probe_kets: Kets on which job circuits were based.
         Returns:
             List of probe states. These are supposed to have dimension equal to the size of Hilbert space, hence if one
             have used tensor products of single-qubit states, then one needs to give here those tensor products. Order
             needs to fit this of results.results.
         """
-
-        circuits_number = sum(len(results.results) for results in results_list)
-
-        # This is a little elaborate, but necessary.
-        qubits_number = int(log(circuits_number, len(probe_kets)))
 
         probe_states = []
 
@@ -185,7 +94,7 @@ class DetectorTomographyFitter:
         return general_tensor_calculator.calculate_tensor_to_increasing_list(probe_states)
 
     @staticmethod
-    def __get_frequencies_array_from_results(results_list: List[Result], qiskit_register_convention) -> np.ndarray:
+    def __get_frequencies_array_from_results(results_list: List[Result]) -> np.ndarray:
         """
         Description:
             Creates an array of frequencies from given qiskit job results. This method is is working with
@@ -232,14 +141,72 @@ class DetectorTomographyFitter:
                     else:
                         normal_order.append(0)
 
-                if qiskit_register_convention:
-                    frequencies = reorder_probabilities(normal_order, range(states_len)[::-1])
-                else:
-                    frequencies = normal_order
+                frequencies = reorder_probabilities(normal_order, range(states_len)[::-1])
 
                 frequencies_array[i][:] = frequencies[:]
 
         return frequencies_array
+
+
+class DetectorTomographyFitter:
+    """
+        This class is meant to resemble qiskit's state tomography and process tomography fitters and to calculate the
+        maximum likelihood povm estimator describing a detector basing on QDT job results and used probe states.
+    """
+
+    def __init__(self, algorithm_convergence_threshold=1e-6):
+        self.algorithmConvergenceThreshold = algorithm_convergence_threshold
+
+    def get_maximum_likelihood_povm_estimator(self, calibration_setup: QDTCalibrationSetup) -> List[np.ndarray]:
+        """
+        Description:
+            Given results of Quantum Detector Tomography experiments and list of probe states, return the Maximum
+            Likelihood estimation of POVM describing a detector. Uses recursive method from [1]. See also [2].
+
+        Parameters:
+            :param calibration_setup:
+
+        Returns
+            Maximum likelihood estimator of POVM describing a detector.
+        """
+
+        number_of_probe_states = calibration_setup.frequencies_array.shape[1]
+        dimension = calibration_setup.probe_states[0].shape[0]
+
+        povm = []
+
+        for j in range(number_of_probe_states):
+            povm.append(np.identity(dimension) / number_of_probe_states)
+
+        # Threshold is dynamic, thus another variable
+        threshold = self.algorithmConvergenceThreshold
+
+        i = 0
+        current_difference = 1
+        while abs(current_difference) >= threshold:
+            i += 1
+
+            if i % 50 == 0:
+                last_step_povm = copy.copy(povm)
+
+            r_matrices = [self.__get_r_operator(povm[j], j, calibration_setup.frequencies_array,
+                                                calibration_setup.probe_states)
+                          for j in range(number_of_probe_states)]
+            lagrange_matrix = self.__get_lagrange_matrix(r_matrices, povm)
+            povm = [self.__calculate_symmetric_m(lagrange_matrix, r_matrices[j], povm[j])
+                    for j in range(number_of_probe_states)]
+
+            if i % 50 == 0:  # calculate the convergence test only sometimes to make the code faster
+                current_difference = sum([np.linalg.norm(povm[k] - last_step_povm[k], ord=2)
+                                          for k in range(number_of_probe_states)])
+
+            elif i > 5e5:  # make sure it does not take too long, sometimes convergence might not be so good
+                threshold = 1e-3
+
+            elif i > 1e5:  # make sure it does not take too long, sometimes convergence might not be so good
+                threshold = 1e-4
+
+        return povm
 
     @staticmethod
     def __get_r_operator(m_m: np.ndarray, index_of_povm_effect: int, frequencies_array: np.ndarray,
@@ -328,61 +295,55 @@ class DetectorTomographyFitter:
 
         return symmetric_m
 
-    # TODO TR: This method may need to be revisited and possibly reduced into several smaller ones.
-    @staticmethod
-    def join_povms(povms: List[List[np.ndarray]], qubit_indices_lists: List[List[int]]) -> List[np.ndarray]:
-        """
-        Description:
-            Generates a POVM from given list of POVMs and qubit indices.
 
-        Parameter:
-            :param povms: List of POVMs corresponding to qubits indices.
-            :param qubit_indices_lists: Indices of qubits for which POVMs were calculated.
+# TODO TR: This method may need to be revisited and possibly reduced into several smaller ones.
+def join_povms(povms: List[List[np.ndarray]], qubit_indices_lists: List[List[int]]) -> List[np.ndarray]:
+    """
+    Description:
+        Generates a POVM from given list of POVMs and qubit indices.
 
-        Return:
-            POVM describing whole detector.
-        """
-        qubits_num = sum([len(indices) for indices in qubit_indices_lists])
+    Parameter:
+        :param povms: List of POVMs corresponding to qubits indices.
+        :param qubit_indices_lists: Indices of qubits for which POVMs were calculated.
 
-        swapped_povms = []
+    Return:
+        POVM describing whole detector.
+    """
+    qubits_num = sum([len(indices) for indices in qubit_indices_lists])
 
-        for i in range(len(qubit_indices_lists)):
-            indices_now = qubit_indices_lists[i]
-            povm_now = povms[i]
+    swapped_povms = []
 
-            # extend povm to higher-dimensional space.by multiplying by complementing identity from right
-            povm_now_extended = [np.kron(Mi, np.eye(2 ** (qubits_num - len(indices_now)))) for Mi in povm_now]
+    for i in range(len(qubit_indices_lists)):
+        indices_now = qubit_indices_lists[i]
+        povm_now = povms[i]
 
-            # begin swapping
-            swapped_povm = copy.copy(povm_now_extended)
+        # extend povm to higher-dimensional space.by multiplying by complementing identity from right
+        povm_now_extended = [np.kron(Mi, np.eye(2 ** (qubits_num - len(indices_now)))) for Mi in povm_now]
 
-            # go from back to ensure commuting
-            for j in range(len(indices_now))[::-1]:
-                index_qubit_now = indices_now[j]
+        # begin swapping
+        swapped_povm = copy.copy(povm_now_extended)
 
-                if index_qubit_now != j:
-                    # swap qubit from jth position to proper position
-                    swapped_povm = [permute_matrix(Mj, qubits_num, (j + 1, index_qubit_now + 1)) for Mj in swapped_povm]
+        # go from back to ensure commuting
+        for j in range(len(indices_now))[::-1]:
+            index_qubit_now = indices_now[j]
 
-            swapped_povms.append(swapped_povm)
+            if index_qubit_now != j:
+                # swap qubit from jth position to proper position
+                swapped_povm = [permute_matrix(Mj, qubits_num, (j + 1, index_qubit_now + 1)) for Mj in swapped_povm]
 
-        # With POVMs now represented by proper matrices (that is, parts corresponding to adequate qubits are now in
-        # desired places), we want to join them.
-        general_tensor_calculator = GeneralTensorCalculator(gtc_matrix_product_counting_function)
-        povm = general_tensor_calculator.calculate_tensor_to_increasing_list(swapped_povms)
+        swapped_povms.append(swapped_povm)
 
-        # We've obtained a POVM, but it is still ordered according to qubit indices. We want to undo that.
-        indices_order = []
-        for indices_list in qubit_indices_lists:
-            indices_order = indices_order + indices_list
+    # With POVMs now represented by proper matrices (that is, parts corresponding to adequate qubits are now in
+    # desired places), we want to join them.
+    general_tensor_calculator = GeneralTensorCalculator(gtc_matrix_product_counting_function)
+    povm = general_tensor_calculator.calculate_tensor_to_increasing_list(swapped_povms)
 
-        new_classical_register = reorder_classical_register(indices_order)
-        sorted_povm = sort_things(povm, new_classical_register)
+    # We've obtained a POVM, but it is still ordered according to qubit indices. We want to undo that.
+    indices_order = []
+    for indices_list in qubit_indices_lists:
+        indices_order = indices_order + indices_list
 
-        return sorted_povm
+    new_classical_register = reorder_classical_register(indices_order)
+    sorted_povm = sort_things(povm, new_classical_register)
 
-
-class QDTCalibrationSetup:
-
-    def __init__(self, qubits_number: int, ):
-        self.qubits_number = qubits_number
+    return sorted_povm
