c2148e311c4cf48026272f161c3e394574faceec
Feigenbaum4669 <osslowik@gmail.com>
2021-03-24 19:49:36 +0100

    Test commit

--- DDOT_module/functions_DDOT_families_heuristics.py
+++ DDOT_module/functions_DDOT_families_heuristics.py
@@ -1,8 +1,8 @@
 """
 Created on 01.03.2021
 
-@author: fbm
-@contact: filip.b.maciejewski@gmail.com
+@author: Filip Maciejewski, Oskar Słowik
+@contact: filip.b.maciejewski@gmail.com, osslowik@gmail.com
 """
 
 
