f439b2be6a38497cb5c3d1a25d93ac40f140e7aa
fbm2718 <filip.b.maciejewski@gmail.com>
2021-11-23 14:46:32 +0100

    hehehe

--- QREM/functions/functions_data_visualization.py
+++ QREM/functions/functions_data_visualization.py
@@ -3,10 +3,11 @@
 @contact: filip.b.maciejewski@gmail.com
 """
 
-import networkx as nx
+# import networkx as nx
 from itertools import chain
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
+import QREM
 
 #arrray_to_print=[[1,2,3,4],[5,6,7],[8,9],[10]]
 
